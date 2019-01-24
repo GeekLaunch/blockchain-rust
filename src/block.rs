782d64ad847583f843e1da1ca18645586f9e5ffc
Jacob <encody@users.noreply.github.com>
2019-01-23 23:53:47 -0600

    second video changes: mining

--- src/block.rs
+++ src/block.rs
@@ -8,21 +8,23 @@ pub struct Block {
     pub prev_block_hash: BlockHash,
     pub nonce: u64,
     pub payload: String,
+    pub difficulty: u128,
 }
 
 impl Debug for Block {
     fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Block[{}]: {} at: {} with: {}",
+        write!(f, "Block[{}]: {} at: {} with: {} nonce: {}",
             &self.index,
             &hex::encode(&self.hash),
             &self.timestamp,
             &self.payload,
+            &self.nonce,
         )
     }
 }
 
 impl Block {
-    pub fn new (index: u32, timestamp: u128, prev_block_hash: BlockHash, nonce: u64, payload: String) -> Self {
+    pub fn new (index: u32, timestamp: u128, prev_block_hash: BlockHash, nonce: u64, payload: String, difficulty: u128) -> Self {
         Block {
             index,
             timestamp,
@@ -30,6 +32,18 @@ impl Block {
             prev_block_hash,
             nonce,
             payload,
+            difficulty,
+        }
+    }
+
+    pub fn mine (&mut self) {
+        for nonce_attempt in 0..(u64::max_value()) {
+            self.nonce = nonce_attempt;
+            let hash = self.hash();
+            if check_difficulty(&hash, self.difficulty) {
+                self.hash = hash;
+                return;
+            }
         }
     }
 }
@@ -43,7 +57,12 @@ impl Hashable for Block {
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
         bytes.extend(self.payload.as_bytes());
+        bytes.extend(&u128_bytes(&self.difficulty));
 
         bytes
     }
+}
+
+pub fn check_difficulty (hash: &BlockHash, difficulty: u128) -> bool {
+    difficulty > difficulty_bytes_as_u128(&hash)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use blockchainlib::*;
 
 fn main () {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block!".to_owned());
+    let mut block = Block::new(0, 0, vec![0; 32], 118318, "Genesis block!".to_owned(), 0x0000ffffffffffffffffffffffffffff);
+
+    block.hash = block.hash();
 
     println!("{:?}", &block);
 
-    let h = block.hash();
-
-    println!("{:?}", &h);
-
-    block.hash = h;
+    block.mine();
 
     println!("{:?}", &block);
 }
